8bf079880e09ad4ac0b3a71dfd8a3fb64f5dd694
Budbark31 <bivinsshilo@gmail.com>
2026-02-09 14:41:14 -0500

    productionBrowserSourceMaps: false,

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // 1. This shrinks the build size (Faster Load)
+  productionBrowserSourceMaps: false,
+
   images: {
     remotePatterns: [
       {
